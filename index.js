91cfbd932b6f3e7aa09edb2a35e8a745643f6179
Jane98721 <144322813+Jane98721@users.noreply.github.com>
2023-11-30 18:07:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const writeEntriesToFile = (entries) => {
 app.post("/submit", (req, res) => {
   const newEntry = {name: req.body.name, number: req.body.number, email: req.body.email, comment: req.body.comment};
   const existingEntries = readEntriesFromFile();
-  existingEntries.push(`${newEntry.name} ${newEntry.number} ${newEntry.email} ${newEntry.comment}`);
+  existingEntries.push(`${newEntry.name} | ${newEntry.number} | ${newEntry.email} | ${newEntry.comment}`);
 
   writeEntriesToFile(existingEntries);
   res.redirect("/");
@@ -36,12 +36,30 @@ let httpServer = app.listen(port, function () {
   console.log(`webbserver körs på port ${port}`);
 });
 
-
-app.post("/postRoute", (req, res)=> {
-const entry= {
-Name: req.body.name,
-Number: req.body.number,
-Email: req.body.email,
-Comment: req.body.comment
-};
+app.get("/", (req, res) => {
+  const entries = readEntriesFromFile();
+  let entriesHTML = "<ul>";
+  entries.forEach((entry) => {
+    entriesHTML += `<li><p>${entry}</p></li>`;
+  });
+  entriesHTML += "</ul>";
+  res.send(`
+      <html>
+        <head>
+          <title>Entries</title>
+        </head>
+        <body>
+          <h1>Entries</h1>
+          <form action="/submit" method="post">
+          <label>Name: </label><input type="text" name="name" /><br />
+          <label>Number: </label><input type="text" name="number" /><br />
+          <label>Email: </label><input type="text" name="email" /><br />
+          <label>Comment: </label><input type="text" name="comment" /><br />
+            <button type="submit">Submit</button>
+          </form>
+          ${entriesHTML}
+        </body>
+      </html>
+    `);
 });
+
